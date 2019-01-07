6dc6d2dc389b6d604718b660932fb4a942d41337
KMB Digital <KMB Digital@KOMBI-0004>
2019-01-07 18:23:44 -0200

    Primary coments layout site manah

---
+++ index.php
@@ -0,0 +1,360 @@
+<!DOCTYPE html>
+<html lang="pt-br" itemscope="" itemtype="http://schema.org/Article">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="index, follow">
+        <link rel="canonical" href="">
+        <link href="themes/manah/image/favicon.png" rel="shortcut icon">                
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta name="keywords" content="Intituto Odontológico">
+        <meta name=" - Intituto Odontológico, sorrir faz bem, Agendamento de Horário, Prevenção e Estética, implantodontia, prótese dentaria, periodontia, odontopediatria e canal."/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="author" content="Junio Santos">       
+        <meta name="author" content="KMB DIGITAL">        
+
+        <title>Manah Instituto Odontológico</title>
+
+        <meta property="og:site_name" content="Manah Instituto Odontológico" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:title" content="Manah Instituto Odontológico | Brasília – DF" />
+        <meta property="og:description" content=" - Intituto Odontológico, sorrir faz bem, Agendamento de Horário, Prevenção e Estética, implantodontia, prótese dentaria, periodontia, odontopediatria e canal." />
+        <meta property="og:image" content="https://manah.com.br/themes/manah/image/site.png" />
+        <meta property="og:url" content="https://manah.com.br" />
+        <meta property="og:type" content="article" />
+        <meta itemprop="name" content="Manah Instituto Odontológico | Brasília – DF">
+        <meta itemprop="description" content=" - Intituto Odontológico, sorrir faz bem, Agendamento de Horário, Prevenção e Estética">
+        <meta itemprop="url" content="https://manah.com.br">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700,800" rel="stylesheet">     
+        <link href="assets/css/estilo.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/jcarousel.responsive.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <header class="main-header container">
+            <div class="info-header content-a">
+                <div class="main-logo">
+                    <a href="#">
+                        <img src="assets/image/logo.png" alt=""/>
+                    </a>
+                </div>
+                <!--CONTATO A-->
+                <div class="main-contact">
+                    <div class="icon-phone">
+                        <img src="assets/image/icon-phone.png" alt=""/>
+                    </div>
+                    <div class="desc-contact-one">
+                        <p>Seg à Sex das 8h as 20h</p>
+                        <p class="p-naumbe"><span>(61)</span><strong> 99999-0000</strong></p>                        
+                    </div>
+                </div>
+                <!--CONTATO B-->
+                <div class="main-contact-cel">
+                    <div class="icon-phone">
+                        <img src="assets/image/icon-phone.png" alt=""/>
+                    </div>
+                    <div class="desc-contact-cel">
+                        <p class="p-naumbe"><span>(61)</span> <strong> 0000-0000</strong></p>
+                        <p>Atendimento via WhatsApp</p>
+                        <p>Seg à Sex das 8h às 20h</p>                        
+                    </div>
+                </div>
+
+                <div class="clear"></div>
+            </div>
+            <div class="bx-nav container">
+                <!--MENU--> 
+                <div class="content-b">
+                    <div class="bx-menu">
+                        <ul class="menu">
+                            <li>
+                                <a  href="#">ESPECIALIDADE</a>
+                            </li>
+                            <li class="manu-active">
+                                <a  href="#">QUEM SOMOS</a>
+                            </li>
+                            <li>
+                                <a  href="#">LOCALIZAÇÃO</a>
+                            </li>
+                        </ul>                       
+                    </div>
+                    <!--AGENDA | REDE SOCIAL-->
+                    <div class="agendamento">
+                        <ul class="menu-agendar">
+                            <li class="bxbtnAgendamento">
+                                <a  href="#">AGENDE SEU HORÁRIO</a>
+                            </li>
+                            <li class="icon-social">
+                                <a  href="#">
+                                    <img src="assets/image/icon-instagram.png" alt=""/>                               
+                                </a>
+                            </li>
+                            <li class="icon-social">
+                                <a  href="#">
+                                    <img src="assets/image/icon-facebook.png" alt=""/>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="clear"></div>
+        </header>
+        <!--FECHA HEADER-->
+        <div class="slide container">
+            <div class="content">        
+                <!--banner rotativos -->        
+                <div class="wrapper">            
+                    <div class="jcarousel-wrapper">
+                        <div class="jcarousel">
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        <img src="assets/image/banner-01.jpg" alt=""/>
+                                    </a>
+                                </li>                                    
+                                <li>
+                                    <a href="#">
+                                        <img src="assets/image/banner-01.jpg" alt=""/>
+                                    </a>
+                                </li>                                    
+                                <li>
+                                    <a href="#">
+                                        <img src="assets/image/banner-01.jpg" alt=""/>
+                                    </a>
+                                </li>                                    
+                            </ul>
+                        </div>
+                        <!--                        <a href="#" class="jcarousel-control-prev">&lsaquo;</a>
+                                                <a href="#" class="jcarousel-control-next">&rsaquo;</a>-->
+                        <p class="jcarousel-pagination"></p>
+                    </div>
+                </div> 
+                <!--banner rotativos -->
+                <div class="clear"></div>
+            </div>    
+        </div>
+        <!--FIM SLIDER-->
+        <main class="main-content container">
+            <hr>                        
+            <section class="sec-pino">               
+                <div class="content-b">
+                    <h1 class="font-zero">Descrições Gerais</h1>
+                    <article class="art-pinos">
+                        <?php
+                        for ($i = 1; $i <= 8; $i++):
+                            ?>
+                            <div class="pins">
+                                <div class="icon-pins">
+                                    <img src="assets/image/icon-tooth.png" alt=""/>
+                                </div>
+                                <div class="desc-pins">
+                                    <h2>Prevenção e Estética</h2> 
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                                        sed do eiusmod tempor incididunt ut labore et
+                                        dolore magna aliqua.
+                                    </p>
+                                </div>
+                            </div>
+                            <?php
+                        endfor;
+                        ?>
+                    </article> 
+                </div>
+                <div class="clear"></div>
+            </section>
+            <!-- FIM PINOS -->
+            <hr>
+            <section class="sec-scheduling container">
+                <h1 class="font-zero">Agendamento de Horários</h1>              
+                <article class="art-scheduling">
+                    <div class="content-b">
+                    <div class="bx-form-scheduling">
+                        <h2>AGENDE SEU HORÁRIO</h2>
+                        <form class="form-scheduling">
+                            <div class="form-group">
+                                <div class="bx-select-row">
+                                    <select class="select-row">
+                                        <option>SELECIONE O TRATAMENTO</option>
+                                        <option>CLAREAMENTO</option>
+                                    </select>
+                                    <span class="icon-schidulen-tooth"><i>
+                                            <img src="assets/image/icon-scheduling-tooth.png" alt=""/>
+                                        </i></span>
+                                </div>
+                                <!--END ROW-CATEGORY-->
+                                <div class="bx-calendar">
+                                    <input type="date" class="date" placeholder="DATA"/>
+                                    <span class="icon-schidulen-calendar"><i>
+                                            <img src="assets/image/icon-scheduling-calendar.png" alt=""/>
+                                        </i></span>
+                                </div>
+                                <!--END ROW DATE-->
+                                <div class="bx-clock">
+                                    <input type="date" class="clock" placeholder="DATA"/>
+                                    <span class="icon-schidulen-clock"><i>                                       
+                                            <img src="assets/image/icon-scheduling-clock.png" alt=""/>
+                                        </i></span>
+                                </div>
+                                <!--END ROW TIME-->
+                            </div>
+                            <!--END FORM GROUP-->
+                            <div class="form-group">
+                                <div class="bx-name">
+                                    <input type="text" class="name" placeholder="SEU NOME">
+                                    <span class="icon-schidulen-name">
+                                        <i>
+                                            <img src="assets/image/icon-scheduling-user.png" alt=""/>
+                                        </i>
+                                    </span>
+                                </div>
+                                <!--END ROW NAME-->
+                                <div class="bx-mail">
+                                    <input type="email" class="mail" placeholder="E-MAIL">
+                                    <span class="icon-schidulen-mail">
+                                        <i>
+                                            <img src="assets/image/icon-scheduling-mail.png" alt=""/>
+                                        </i>
+                                    </span>
+                                </div>
+                                <!--END ROW EMAIL-->
+                            </div>
+                            <!--END FORM GROUP-->
+                            <div class="form-group">
+                                <div class="bx-phone">
+                                    <input type="tel" class="phone" placeholder="TELEFONE">
+                                    <span class="icon-schidulen-phone">
+                                        <i>
+                                            <img src="assets/image/icon-scheduling-phone.png" alt=""/>
+                                        </i>
+                                    </span>
+                                </div>
+                                <!--END ROW PHONE-->
+                                <div class="bx-btn">
+                                    <input type="submit" class="btnMarcar" value="MARCAR">                                   
+                                </div>
+                                <!--END BOTAO MARCAR-->
+                            </div>
+                            <!--END FORM GROUP-->
+                        </form>
+                    </div>
+                    <!--FIM BOX FORMULARIO-->
+                </div>
+                </article>
+                <!--FIM ARTICLE AGENDAMENTO-->
+            </section>
+            <!--FIM SECTION AGENDAMENTO-->
+            <section class="sec-about container">
+                <h1 class="font-zero">QUEM É MANAH</h1>
+                <div class=""></div>
+
+                <hr>
+                <div class="box-container-about">
+                    <div class="content-b">
+                        <article class="art-about">
+                            <div class="bx-about">
+                                <div class="thumb-about">
+                                    <img src="assets/image/banner-about01.jpg" alt=""/>
+                                </div>
+                                <div class="thumb-about">
+                                    <img src="assets/image/banner-about02.jpg" alt=""/>
+                                </div>
+                            </div>
+                            <div class="bx-about">                                
+                                <div class="desc-about">
+                                    <div class="title-about">
+                                        <h2>QUEM SOMOS</h2>
+                                    </div>
+                                    <div class="text-about">
+                                        <p>
+                                            O Instituto Manah conta com uma equipe de profissionais qualificados em todas as especialidades para melhor lhe atender. Nosso objetivo é focar na prevenção, cuidando da saúde bucal e também intervir quando necessário, para devolver o bem estar ao paciente já debilitado, melhorando dua estética e função. Acima de tudo, proporcionar saúde bucal e qualidade de vida ao paciente.                                            
+                                        </p>
+                                        <p>                                            
+                                            Nossa missão é preservar a saúde, o sorriso e a qualidade de vida das pessoas, a partir de um atendimento profissional, ético e personalizado visando a satisfação e bem-estar do cliente.
+                                        </p>
+                                    </div>
+                                    descricao
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+                <hr>
+                <div class="clear"></div>
+            </section>
+            <!--END ABOUT MANAT-->
+            <div class="clear"></div>
+        </main>
+        <!--FIM CONTENT-->
+        <footer class="main-footer container">
+            <section class="sec-contact">
+                <h1 class="font-zero">Contato Manah</h1>
+                <article class="art-contact">
+                    <div class="bx-contact">
+                        <h2>CONTATO</h2>
+                        <div class="bx-form-contact">
+                            <form class="form-contact">
+                                <div class="form-group">
+                                    <input type="text" class="name" placeholder="seu nome"/>
+                                    <img src="assets/image/icon-scheduling-user.png" alt=""/>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row-mail">
+                                        <input type="email" class="mail" placeholder="e-mail"/>
+                                        <img src="assets/image/icon-scheduling-mail.png" alt=""/>
+                                    </div>
+                                    <div class="row-phone">
+                                        <input type="tel" class="phone" placeholder="telefone"/>
+                                        <img src="assets/image/icon-scheduling-phone.png" alt=""/>
+                                    </div>
+                                </div>
+
+                                <div class="row-description">
+                                    <textarea rows="5" class="description" cols="20">
+                                        
+                                    </textarea>
+                                    <img src="assets/image/icon-form-pencil.png" alt=""/>
+                                </div>
+                                <div class="row-btn-contact">
+                                    <input type="submit" class="btn-contact" value="ENVIAR"/>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </article>
+            </section>
+            <!--LOCALIZACAO MANAH-->
+            <section  class="container">
+                <article class="location-map">
+                    <div class="map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7458.158241451372!2d-48.05059998079494!3d-15.838224512308425!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd5d09edfe150d05c!2sManah+Instituto+odontol%C3%B3gico!5e0!3m2!1spt-BR!2sbr!4v1546888387172" width="100%" height="470" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        <div class="clear"></div>
+                    </div>
+                </article>
+            </section>
+            <!--FIM LOCALIZACAO MANAH-->
+            <div class="bx-copy container">
+                <div class="copy">
+                    <p>&COPY; <?php echo date('Y'); ?><a href="#">Desenvolvido por KMB Digital</a></p>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <!--FIM CONTACT PAGE-->
+        </footer>
+        <!--FIM RODAPE-->
+        <script src="assets/js/jquery.js" type="text/javascript"></script>
+        <script src="assets/js/jquery.jcarousel.min.js" type="text/javascript"></script>
+        <script src="assets/js/jcarousel.responsive.js" type="text/javascript"></script>
+        <script language=javascript type="text/javascript">
+//            dayName = new Array("domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado")
+//            monName = new Array("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
+//            now = new Date
+        </script>
+    </head>
+<body>
+
+</body>
+</html>
+
